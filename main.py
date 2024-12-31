86e2afc5c07c9d1c1bcdaa1a6ec69e220da6bf79
Wagner Herculano <wagnerhs@ciandt.com>
2024-12-31 10:50:04 -0300

    Added fake database and route list_books

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
+from typing import List, Dict, Union
+
 from fastapi import FastAPI
 
 
 app = FastAPI()
 
+FAKE_BOOKS_DATABASE: List[Dict[str, Union[int, str]]] = [
+    {'id': 1, 'title': 'Learning Python: Powerful Object-Oriented Programming'},
+    {'id': 2, 'title': 'Think Python: An Introduction to Software Design'},
+    {'id': 3, 'title': 'Fluent Python: Clear, Concise, and Effective Programming'},
+    {'id': 4, 'title': 'Web Scraping with Python: Collecting More Data from the Modern Web'},
+    {'id': 5, 'title': 'Automate the Boring Stuff with Python, 2nd Edition: Practical Programming for Total Beginners'},
+    {'id': 6, 'title': 'Classic Computer Science Problems in Python'},
+]
 
-@app.get('/')
-async def root():
-    return {'message': 'Hello FastAPI World!'}
+
+@app.get('/', status_code=200)
+async def welcome():
+    """Welcome to my Bookstore"""
+    return {'message': 'Welcome to my Bookstore'}
+
+
+@app.get('/books', status_code=200)
+async def list_books():
+    """Lists all books"""
+    return {'books': FAKE_BOOKS_DATABASE}
